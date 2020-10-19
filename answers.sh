c88416c376ffc77a5f125b70b353a7a5f84077d8
AmiraMoussa-code <amira.moussa7197@gmail.com>
2020-10-19 10:10:38 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,23 @@
+"select * from students"
+
+1-"SELECT name FROM students;"
+
+2-"SELECT * FROM students
+WHERE Age > 30;"
+
+3-"SELECT * FROM students
+WHERE Age = 30 AND Gender = 'f;"
+
+4-"SELECT Points FROM students
+WHERE name='Alex';"
+
+5-"INSERT INTO students(name , Age , Gender, Points)
+VALUES ('Amira' , 23 , 'F' , 150)"
+
+6-'UPDATE students
+SET Points = 400
+WHERE name= "Basma" '
+
+7- 'UPDATE students
+SET Points = 100
+WHERE name = "Alex"'
